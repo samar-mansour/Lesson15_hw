8ff98a3f90c1cb9c3f0e2f7f28e01f7e310857de
samar-mansour <52625367+samar-mansour@users.noreply.github.com>
2020-06-29 15:15:34 +0300

    Add files via upload

---
+++ Lesson15_hw/Lesson15_hw/Game.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson15_hw
+{
+    class Game
+    {
+        public string _name;
+        public int _numberOfPlayers;
+        public float _rating;
+        public string _originCountry;
+
+        public Game()
+        {
+        }
+
+        public Game(string name, float rating = 5.5f)
+        {
+            _name = name;
+            _rating = rating;
+        }
+
+        public Game(string name, int numberOfPlayers, float rating, string originCountry) : this(name)
+        {
+            _numberOfPlayers = numberOfPlayers;
+            _rating = rating;
+            _originCountry = originCountry;
+        }
+
+        public Game(string name, string originCountry)
+        {
+            _name = name;
+            _originCountry = originCountry;
+        }
+
+        public int TellMeHowManyPlayers()
+        {
+            return _numberOfPlayers;
+        }
+
+        public override string ToString()
+        {
+            return $"{base.ToString()}\n Game: {_name} Players: {_numberOfPlayers} Rating: {_rating} Country: {_originCountry}";
+        }
+    }
+}
---
+++ Lesson15_hw/Lesson15_hw/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson15_hw
+{
+    class Program
+    {
+        static void PrintGame(Game g)
+        {
+            int number = g.TellMeHowManyPlayers();
+            Console.WriteLine($"Game Info:\n {g} Players number: {number}");
+        }
+        static void Main(string[] args)
+        {
+            Game dmc = new Game("DMC-5", 20, 5.6f, "Japan");
+            Game forza = new Game("Forza");
+            Game needForSpeed = new Game()
+            {
+                _name = "Need For Speed",
+                _numberOfPlayers = 2,
+                _originCountry = "Spain"
+            };
+            needForSpeed._rating = 7.8f;
+            Game ashpalt9 = new Game("Asphalt-9", "Mexico");
+
+            PrintGame(dmc);
+            PrintGame(forza);
+            PrintGame(needForSpeed);
+
+        }
+    }
+}
